ed542f6cc5ca50deefce82eae5c977e9899d81aa
abhishekgupta1906 <21cs3001@rgipt.ac.in>
2024-11-08 18:16:34 +0530

    backend completed

--- main.go
+++ main.go
@@ -1,71 +1,129 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Action struct {
-	ID        int    `json:"id"`
+	ID        primitive.ObjectID     `json:"id"`
 	Completed bool   `json:"completed"`
 	Body      string `json:"body"`
 }
 
+
+var collection *mongo.Collection
+
 func main() {
-	fmt.Println("Hello, world")
-	app := fiber.New()
-	actions := []Action{}
-	err :=godotenv.Load(".env")
+	fmt.Println("Server,Running")
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	PORT := os.Getenv("PORT")
-	app.Get("/api/actions", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(actions)
-	})
-	app.Post("/api/actions", func(c *fiber.Ctx) error {
-		action := new(Action)
-		if err := c.BodyParser(action); err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": err.Error()})
-		}
-		if action.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error": "Body is required"})
-		}
-		action.ID = len(actions) + 1
-		actions = append(actions, *action)
-		return c.Status(201).JSON(action)
-	})
-	// update action
-	// fm.sprint to convert int to string
-	app.Patch("/api/actions/:id", func(c *fiber.Ctx) error {
-		id :=c.Params("id")
-		for i,action := range actions{
-			if fmt.Sprint(action.ID) == id{
-				
-				actions[i].Completed = !action.Completed
-				return c.Status(200).JSON(actions[i])
-			}
-		}
-		return c.Status(404).JSON(fiber.Map{"error":"Action not found"})
 
-	})
-	// delete action
-	app.Delete("/api/actions/:id", func(c *fiber.Ctx) error {
-		id :=c.Params("id")
-		for i,action := range actions{
-			if fmt.Sprint(action.ID) == id{
-				
-				actions=append(actions[:i],actions[i+1:]...)
-				return c.Status(200).JSON(fiber.Map{"success":true})	
-			}
+	MONGODB_URL := os.Getenv("MONGODB_URL")
+	//  connect to mongodb
+	clientoptions := options.Client().ApplyURI(MONGODB_URL)
+	client, err := mongo.Connect(context.Background(), clientoptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(context.Background())
+	err = client.Ping(context.Background(), nil)
+	fmt.Println("Connected to MongoDB")
+	collection = client.Database("actionhub").Collection("actions")
+
+	app := fiber.New()
+	app.Get("/api/actions",getActions)
+	app.Post("/api/actions",createAction)
+	app.Patch("/api/actions/:id",updateAction)
+	app.Delete("/api/actions/:id",deleteAction)
+	port := os.Getenv("PORT")
+	log.Fatal(app.Listen("0.0.0.0:" + port))
+}
+
+func getActions(c *fiber.Ctx) error {
+	var actions []Action
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	// bson.M is used for filtering
+	if err != nil {
+		c.Status(500).SendString(err.Error())
+		return err
+	}
+	defer cursor.Close(context.Background())
+	for cursor.Next(context.Background()) {
+		// cursor collection pr iterate kr rha h
+		var action Action
+		if err := cursor.Decode(&action); err != nil {
+			return err
 		}
-        return c.Status(404).JSON(fiber.Map{"error":"Action not found"})
+
+		actions = append(actions, action)
+	}
+	return c.JSON(actions)
+}
+func createAction(c *fiber.Ctx) error {
+	action:=new(Action)
+	if err:=c.BodyParser(action);err!=nil{
+		return err
+	}
+	if(action.Body==""){
+		c.Status(400).JSON(fiber.Map{"error":"Body cannot be empty"})
+		return nil
+	}
+	insertResult,err:=collection.InsertOne(context.Background(),action)
+	if err!=nil{
+
+	}
+	action.ID=insertResult.InsertedID.(primitive.ObjectID)
+	return c.Status(201).JSON(action)
+}
+
+func updateAction(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+	}
+	filter := bson.M{"_id": objectID}
+
+	var action Action
+	err = collection.FindOne(context.Background(), filter).Decode(&action)
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{"error": "Action not found"})
+	}
+
+	update := bson.M{"$set": bson.M{"completed": !action.Completed}}
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Failed to update action"})
+	}
+
+	return c.JSON(fiber.Map{"message": "Action updated successfully"})
+}
+
+func deleteAction(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+	}
+	filter := bson.M{"_id": objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Action deleted successfully",
+		"success": true,
 	})
-
-
-	log.Fatal(app.Listen(":"+PORT))
 }
